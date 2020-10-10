beac54fa696f2383998269de2eae2aceadeb6bf0
Vinícius R. Migliorança <vinicius@ecocentauro.com.br>
2020-10-10 09:40:01 -0300

    Primeiro exemplo.

---
+++ aspnetcore/Exemplo1/Exemplo1/Controllers/ProdutoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exemplo1.Controllers
+{
+    /// <summary>
+    /// A herança de 'controllerbase' indica o utilização do recurso básico para utilização da entidade como
+    /// um 'controller'. Casos em que o asp.net é utilizado no frontend, a herança seria de 'controller'.
+    /// </summary>
+    [ApiController]
+    //[Route("api/[controller]")] - Reza que a boa prática pede para especificar o nome (por isso logo abaixo).
+    [Route("api/produtos")]
+    public class ProdutoController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult ObterTodos()
+        {
+            return Ok("Todos os produtos.");
+        }
+    }
+}
